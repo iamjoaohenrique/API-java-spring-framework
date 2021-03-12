6d639cb7abd0ba9a636e979f357715a72ea9a1f9
iamjoaohenrique <joao.silva@mobbuy.com.br>
2021-03-12 00:52:26 -0300

    Aula 2 curso de api rest com spring framework até a metade da aula 2

--- README.md
+++
@@ -1,1 +0,0 @@
-# Curso-java-spring-framework-
--- src/main/java/com/example/ordemdeservicos/api/controller/ClienteController.java
+++ src/main/java/com/example/ordemdeservicos/api/controller/ClienteController.java
@@ -1,31 +1,77 @@
 package com.example.ordemdeservicos.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ordemdeservicos.domain.model.Cliente;
+import com.example.ordemdeservicos.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("clientes")
 public class ClienteController {
-	
-	@GetMapping("/clientes")
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@GetMapping
 	public List<Cliente> Listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("joao henrique");
-		cliente1.setTelefone("11 95928-4227");
-		cliente1.setEmail("joao@hotmail.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("maria jose");
-		cliente2.setTelefone("11 91234-1234");
-		cliente2.setEmail("maria@hotmail.com");
-		
-		return Arrays.asList(cliente1,cliente2);
+		return clienteRepository.findAll();
 	}
 
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(
+			@Valid
+			@PathVariable Long clienteId, 
+			@RequestBody Cliente cliente) {
+		
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
